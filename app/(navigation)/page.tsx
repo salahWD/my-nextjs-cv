d570903c913f242042123f4dace78be9aed67ff4
salah bakhash <salahb170@gmail.com>
2024-09-09 23:59:04 +0300

    fix responsiveness in mobile screen

--- app/(navigation)/page.tsx
+++ app/(navigation)/page.tsx
@@ -13,13 +13,13 @@ export default function Home() {
             <div className="rounded-3xl bg-gradient-radial from-red-900 to-transparent p-6 origin-bottom animate-scale-fade-in opacity-0">
               <img src={"./salah.jpg"} className="border-spacing-3 bg-no-repeat bg-center border-red-950 border rounded-full object-cover" width="150" alt="salahbakhash logo" />
             </div>
-          <h1 className="py-3.5 px-0.5 z-10 text-7xl leading-[0rem] opacity-0 text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display whitespace-nowrap bg-clip-text ">
+          <h1 className="py-3.5 px-0.5 z-10 text-4xl lg:text-7xl leading-[0rem] opacity-0 text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display whitespace-nowrap bg-clip-text ">
             salahbakhash
           </h1>
 
           <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
           <div className="my-8 text-center animate-fade-in">
-            <h2 className="text-base text-zinc-500">
+            <h2 className="text-base text-zinc-500 max-md:px-6">
               I'm showing{" "}
               <Link
                 href="#projects"
